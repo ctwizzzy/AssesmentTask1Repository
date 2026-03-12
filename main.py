423c1e9a9792d7da7cf95a42f25cd01c15d62184
ctwizzy <aaravrajkumar852@gmail.com>
2026-03-13 10:10:45 +1100

    football api
    changed api to a sports theme

--- main.py
+++ main.py
@@ -1,36 +1,24 @@
 import requests
 
-# Base URL for NASA's Astronomy Picture of the Day (APOD) API
-API_URL = "https://api.nasa.gov/planetary/apod"
-API_KEY = "YOUR_KEY"  # Replace with your NASA API key
 
-def fetch_apod(date=None):
-    """Fetch NASA's Astronomy Picture of the Day (APOD)."""
-    params = {"api_key": API_KEY}
-    if date:
-        params["date"] = date  # Format: YYYY-MM-DD
-    
-    response = requests.get(API_URL, params=params)
-    
-    if response.status_code == 200:
-        data = response.json()
-        return {
-            "title": data.get("title", "Unknown"),
-            "date": data.get("date", "Unknown"),
-            "explanation": data.get("explanation", "No description available."),
-            "image_url": data.get("url", "No image available")
-        }
-    else:
-        print("Error fetching data from NASA API.")
-        return None
 
-def display_apod(apod_data):
-    """Display APOD details."""
-    if apod_data:
-        print(f"Title: {apod_data['title']}")
-        print(f"Date: {apod_data['date']}")
-        print(f"Description: {apod_data['explanation'][:200]}...")
-        print(f"Image URL: {apod_data['image_url']}")
-    else:
-        print("No data to display.")
-       
+BASE_URL = "https://v3.football.api-sports.io/teams"
+
+KEY="6d4677aed879c7c68f81b280ae6099c9"
+
+
+headers = {
+        'x-apisports-key': KEY
+    }
+
+params = {
+    'id': '33'
+}
+
+
+response = requests.get(BASE_URL, headers=headers, params=params )
+
+if response.status_code == 200:
+   print(response.json())
+else:
+    print(f"Error {response.status_code}")
