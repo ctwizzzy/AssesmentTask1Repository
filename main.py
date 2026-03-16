9defd4d0aaf6aa68a5b4914f456a66c75e73c5af
ctwizzy <aaravrajkumar852@gmail.com>
2026-03-16 11:05:38 +1100

    update on code
    updated code

--- main.py
+++ main.py
@@ -1,24 +1,20 @@
 import requests
-
-
+import pandas as pd
 
 BASE_URL = "https://v3.football.api-sports.io/teams"
-
+# API key
 KEY="6d4677aed879c7c68f81b280ae6099c9"
-
 
 headers = {
         'x-apisports-key': KEY
     }
-
+# The question the terminal will ask the user when the code is run
 user_wants_team = input("What team you would like to find").capitalize()
 
-
-
+# 
 params = {
    "name": user_wants_team
 }
-
 
 response = requests.get(BASE_URL, headers=headers, params=params )
 
@@ -30,3 +26,7 @@ elif response.status_code == 200:
    print(response.json())
 else:
     print(f"Error {response.status_code}")
+
+def print_info(data):
+    print(f'Team Name is {data["name"]}')
+    
