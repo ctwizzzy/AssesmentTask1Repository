cb86dc6d354fddcfef3f6fb381d874109f5ca1ec
ctwizzy <aaravrajkumar852@gmail.com>
2026-03-22 19:40:15 +1100

    Removed old trial code

--- main.py
+++ main.py
@@ -1,35 +1,3 @@
-# import requests
-# import pandas as pd
-
-# BASE_URL = "https://v3.football.api-sports.io/teams"
-# # API key
-# KEY="6d4677aed879c7c68f81b280ae6099c9"
-
-# headers = {
-#         'x-apisports-key': KEY
-#     }
-# # The question the terminal will ask the user when the code is run
-# user_wants_team = input("What team you would like to find").capitalize()
-
-# # 
-# params = {
-#    "name": user_wants_team
-# }
-
-# response = requests.get(BASE_URL, headers=headers, params=params )
-
-# data = response.json()
-
-# if data.get('results') == 0:
-#     print("Failed to find an accurate result check your spelling and fix it for a result")
-# elif response.status_code == 200:
-#    print(response.json())
-# else:
-#     print(f"Error {response.status_code}")
-
-# def print_info(data):
-#     print(f'Team Name is {data["name"]}')
-    
 import requests
 import pandas as pd
 import matplotlib.pyplot as plt
